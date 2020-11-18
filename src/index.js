498b1ee1c68801054688d151766823a3c54c212c
alexangria1992 <alex040892@gmail.com>
2020-11-18 16:02:23 +1300

    nested components

--- src/index.js
+++ src/index.js
@@ -1,37 +1,21 @@
 import React from 'react' 
 import ReactDom from 'react-dom';
 
-//JSX RULES
-// return single element
-// div/section/article or Fragment
-// use camelCase for html attribute
-// className instead of class
-// close every element
-// formatting
-
-// function Greeting()
-// {
-//   return <h4>This is alex and this is my first component</h4>
-// }
-
-// const Greeting = () => {
-//   return React.createElement('h1', {}, 'Hello world');
-// }
 
 function Greeting()
 {
   return(
     <div>
-      <h3>hello people</h3>
-      <ul>
-        <li>
-          <a href="#">Hello World</a>
-        </li>
-        <img src="" alt=""/>
-        <input type="text"/>
-      </ul>
+      <Person/>
+      <Message/>
     </div>
   )
+}
+
+const Person = () =>  <h2>John Doe</h2>
+const Message = ()=> {
+  return <p>This is my message</p>
+
 }
 ReactDom.render(<Greeting/>, document.getElementById('root'));
 
