7c6b33ee1f176dec2c240075c491d82ed8c3c177
alexangria1992 <alex040892@gmail.com>
2020-11-18 13:48:29 +1300

    deployed react app to github pages

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import profileImage from './img/1.jpg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>My React App </h1>
+      <h2>Author: Alex Angrignon</h2>
+      <img src={profileImage} alt="profile-image"/>
+    </div>
+  );
+}
+
+export default App;
