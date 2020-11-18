b66c2f42797c03940a7568fb8a5a6b992693fae3
alexangria1992 <alex040892@gmail.com>
2020-11-18 15:54:49 +1300

    JSX RULES

--- src/index.js
+++ src/index.js
@@ -1,9 +1,37 @@
 import React from 'react' 
 import ReactDom from 'react-dom';
 
+//JSX RULES
+// return single element
+// div/section/article or Fragment
+// use camelCase for html attribute
+// className instead of class
+// close every element
+// formatting
+
+// function Greeting()
+// {
+//   return <h4>This is alex and this is my first component</h4>
+// }
+
+// const Greeting = () => {
+//   return React.createElement('h1', {}, 'Hello world');
+// }
+
 function Greeting()
 {
-  return <h4>This is alex and this is my first component</h4>
+  return(
+    <div>
+      <h3>hello people</h3>
+      <ul>
+        <li>
+          <a href="#">Hello World</a>
+        </li>
+        <img src="" alt=""/>
+        <input type="text"/>
+      </ul>
+    </div>
+  )
 }
-
 ReactDom.render(<Greeting/>, document.getElementById('root'));
+
